917ce3453955a79fd9abb08175ff67eb117d2c75
rdk <radoslaw.janicki@monterail.com>
2015-10-28 20:50:25 +0100

    Fix unique id issue

--- db/migrate/20151028181941_add_list_id_to_tasks.rb
+++ db/migrate/20151028181941_add_list_id_to_tasks.rb
@@ -1,6 +1,6 @@
 class AddListIdToTasks < ActiveRecord::Migration
   def change
     add_column :tasks, :list_id, :integer
-    add_index :tasks, :list_id, :unique => true
+    add_index :tasks, :list_id
   end
 end
